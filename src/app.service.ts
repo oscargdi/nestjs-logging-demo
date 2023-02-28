dc625edc843935f73d8b05319aebc96eea334069
Oscar Diaz <oscargdi@hotmail.com>
2023-02-28 21:34:39 +0000

    chore: use pino logger in service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectPinoLogger(AppService.name) private readonly logger: PinoLogger,
+  ) {}
+
   getHello(): string {
+    this.logger.info({ operation: 'getHello', result: 'success' });
     return 'Hello World!';
   }
 }
