f5e2ccc27bff7a5e344cb2d16143ceb97ec4cad6
Oscar Diaz <oscargdi@hotmail.com>
2023-02-28 21:46:56 +0000

    chore: use recommended error interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from 'nestjs-pino';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
   await app.listen(3000);
 }
 bootstrap();
